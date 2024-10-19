29714ea7bab656180237cbf2ecb97c09feed8c56
MbusyaNgesa <theengesa@gmail.com>
2024-10-19 14:39:06 +0300

    Nabar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+
+//JSX Crash Course
+// const App = () => {
+//   const name = "Ngesa";
+//   const x = 23;
+//   const y = 2;
+//   const names = ["Ngesa", "Malick", "Willy", "Virgil"];
+
+//   return (
+//     <>
+//       <div className="text-red-700 text-4xl">App</div>
+//       <p>Hello {name} </p>
+//       <p>
+//         Multiple of {x} and {y} is {x * y}
+//       </p>
+//       <ul>
+//         {names.map((name, index) => (
+//           <li key={index}>{name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default App;
 
 const App = () => {
-  return <div className="text-red-700 text-4xl">App</div>;
+  return <Navbar />;
 };
 
 export default App;
