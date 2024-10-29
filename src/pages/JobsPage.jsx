f7633a24a175279f1abd9e7c9bf4ecf29f0e5754
MbusyaNgesa <theengesa@gmail.com>
2024-10-29 23:18:21 +0300

    JSON Server Setup

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,4 +1,10 @@
+import JobListings from "../components/JobListings";
+
 const JobsPage = () => {
-  return <h1>JobsPage</h1>;
+  return (
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListings />
+    </section>
+  );
 };
 export default JobsPage;
