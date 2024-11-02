314879cd8e46e954a18348181551b56ea8b3c00a
MbusyaNgesa <theengesa@gmail.com>
2024-11-02 16:21:05 +0300

    useParams() to fetch id

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NOtFoundPage";
+import JobPage from "./pages/JobPage";
 
 //JSX Crash Course
 // const App = () => {
@@ -40,6 +41,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
